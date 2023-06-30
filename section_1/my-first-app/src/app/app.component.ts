2e117e41efca94c8997f0b528d10d48f7117d04f
whittlialleman <alle.alleman@gmail.com>
2023-06-30 11:53:00 -0600

    Finished video 22

--- section_1/my-first-app/src/app/app.component.ts
+++ section_1/my-first-app/src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: darkblue;
+  }`]
 })
 export class AppComponent {
 }
