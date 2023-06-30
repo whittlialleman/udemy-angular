9ba6533f454636d7f896763fba71dc504be444e2
whittlialleman <alle.alleman@gmail.com>
2023-06-30 09:18:44 -0600

    Created starter files and completed videos 486-489

---
+++ section_34/basics.ts
@@ -0,0 +1,17 @@
+//Primitives: numbers, strings, booleans
+//More complex types: arrays, objects
+//Function types, parameters
+
+//Primitives
+
+let age: number; //have to use lowercase so it doesn't point the JS Number object
+
+age = 25;
+
+let userName: string;
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+//
