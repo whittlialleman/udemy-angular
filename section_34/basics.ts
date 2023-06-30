82c7d423a1a954c12d56f0909179c7aeba57a63c
whittlialleman <alle.alleman@gmail.com>
2023-06-30 09:41:42 -0600

    Finished video 495

--- section_34/basics.ts
+++ section_34/basics.ts
@@ -51,3 +51,15 @@ function add(a:number, b: number): number { //You can declare a type here, or TS
 function print_output(value: any) { //Because this isn't returning anything, it's return type is void
     console.log(value);
 };
+
+//Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+};
+
+const demoArray = [1,2,3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); //[-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
