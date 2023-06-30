9d94612b62208ad7ddfdac1047cf26644e75f73b
whittlialleman <alle.alleman@gmail.com>
2023-06-30 09:32:03 -0600

    Finished videos 492 and 493

--- section_34/basics.ts
+++ section_34/basics.ts
@@ -20,21 +20,24 @@ let hobbies: string[]; //This allows you to have an array of strings instead of 
 
 hobbies = ['Motorcycle riding', 'Cooking', 'Watching TV'];
 
-let person: {
+type Person = { //Type aliases - used on person and people variables below
     name: string;
     age: number;
-}; 
+};
+
+let person: Person; 
 
 person = {
     name: 'Whip',
     age: 25
 };
 
-let people: {
-    name: string;
-    age: number;
-}[];
+let people: Person[];
 
 //Type inference
 let course = 'React - The Complete Guide'; //Type inference is helpful and should be used
 //course = 12341; //Get an error because typescript inferred the type of course is a string, not a number
+
+//Union types
+let course2: string | number = 'React - The Complete Guide';
+course2 = 12341; //No longer get the error because we allow both string and number types for the variable
--- section_34/with-typescript.ts
+++ section_34/with-typescript.ts
@@ -1,6 +1,6 @@
 function add(a: number, b: number) {
     return a + b;
-}
+};
 
 const result = add(2, 5);
 
