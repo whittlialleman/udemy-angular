6864289b3c955b1fb62090e21e68e82adab83f49
whittlialleman <alle.alleman@gmail.com>
2023-07-06 08:53:58 -0600

    Finished video 24 - assignment 1 - also corrected some errors from previous videos

--- section_1/my-first-app/src/app/app.component.ts
+++ section_1/my-first-app/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  //selector: '[app-servers]',
-  //selector: '.app-servers',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
-  h3 {
-    color: darkblue;
-  }`]
+  styleUrls: ['./app.component.css']
+  // styles: [`
+  // h3 {
+  //   color: darkblue;
+  // }`]
 })
 export class AppComponent {
 }
--- section_1/my-first-app/src/app/servers/servers.component.ts
+++ section_1/my-first-app/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
   selector: 'app-servers',
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
