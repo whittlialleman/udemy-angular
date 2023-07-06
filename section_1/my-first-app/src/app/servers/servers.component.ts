a153745726831704ff42b128f470f1a199cfa202
whittlialleman <alle.alleman@gmail.com>
2023-07-06 09:07:56 -0600

    Finished video 27

--- section_1/my-first-app/src/app/servers/servers.component.ts
+++ section_1/my-first-app/src/app/servers/servers.component.ts
@@ -4,9 +4,15 @@ import { Component } from '@angular/core';
   //selector: '[app-servers]',
   //selector: '.app-servers',
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 }
