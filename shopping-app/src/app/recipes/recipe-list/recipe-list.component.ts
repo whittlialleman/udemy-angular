8b8f788243a70aff9f96475a6d1925e18dfe766b
whittlialleman <alle.alleman@gmail.com>
2023-07-06 11:51:51 -0600

    Finished video 55

--- shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
   ];
 
 }
