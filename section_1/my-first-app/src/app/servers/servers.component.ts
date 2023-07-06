ab147855c9dc7667b20218892d72f4753f84c3ef
whittlialleman <alle.alleman@gmail.com>
2023-07-06 09:24:03 -0600

    Finished video 33

--- section_1/my-first-app/src/app/servers/servers.component.ts
+++ section_1/my-first-app/src/app/servers/servers.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = '';
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
