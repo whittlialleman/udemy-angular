65be8b83ccb65e8187307a725f4426627b7ffc36
whittlialleman <alle.alleman@gmail.com>
2023-07-06 09:53:34 -0600

    Finished video 37

--- section_1/my-first-app/src/app/servers/servers.component.ts
+++ section_1/my-first-app/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -19,7 +20,8 @@ export class ServersComponent {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
