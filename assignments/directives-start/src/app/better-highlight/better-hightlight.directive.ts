90fd9bc89a4e07c63a0364db35622273c875f40d
whittlialleman <alle.alleman@gmail.com>
2023-07-10 12:41:30 -0600

    Finished video 97

--- assignments/directives-start/src/app/better-highlight/better-hightlight.directive.ts
+++ assignments/directives-start/src/app/better-highlight/better-hightlight.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHightlight]'
 })
 export class BetterHightlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.color') color: string = 'black';
   
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
   
@@ -15,10 +17,14 @@ export class BetterHightlightDirective implements OnInit {
   @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.backgroundColor = 'blue';
+    this.color = 'white';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    this.backgroundColor = 'transparent';
+    this.color = 'black';
   }
 }
