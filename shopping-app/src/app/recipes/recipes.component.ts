db4813789320c257a3386d1073078cba3bf423d4
whittlialleman <alle.alleman@gmail.com>
2023-07-10 15:46:35 -0600

    Finished video 119

--- shopping-app/src/app/recipes/recipes.component.ts
+++ shopping-app/src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
@@ -10,4 +10,14 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent {
   selectedRecipe: Recipe;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe
+      }
+    );
+  }
 }
