30c8d8372818e4357fe2de5bec401a6bfda49118
whittlialleman <alle.alleman@gmail.com>
2023-07-10 14:41:57 -0600

    Finished video 113

--- assignments/services-start/src/app/account/account.component.ts
+++ assignments/services-start/src/app/account/account.component.ts
@@ -17,5 +17,6 @@ export class AccountComponent {
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
     //this.loggingService.logStatusChange(status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- assignments/services-start/src/app/new-account/new-account.component.ts
+++ assignments/services-start/src/app/new-account/new-account.component.ts
@@ -10,7 +10,11 @@ import { AccountsService } from '../accounts.service';
 })
 export class NewAccountComponent {
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {}
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
