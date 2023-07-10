1cf571d53b30ef64fc6d80e2dcd3a596af7fc8d4
whittlialleman <alle.alleman@gmail.com>
2023-07-10 16:05:50 -0600

    Finished video 122

--- shopping-app/src/app/recipes/recipe.service.ts
+++ shopping-app/src/app/recipes/recipe.service.ts
@@ -1,13 +1,15 @@
 import { EventEmitter } from '@angular/core'
 
 import { Recipe } from './recipe.model'
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-        new Recipe('Another Test Recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+        new Recipe('Schnitzel', 'German schnitzel dish', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
+
+        new Recipe('Cheeseburger', 'Traditional American cheeseburger', 'https://cdn.pixabay.com/photo/2023/03/03/02/27/food-7826772_1280.jpg', [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]),
       ];
 
     getRecipes() {
