5ba735748118edda9cb312e0cb3dca8372717de1
whittlialleman <alle.alleman@gmail.com>
2023-07-10 12:54:47 -0600

    Finished video 98

--- assignments/directives-start/src/app/better-highlight/better-hightlight.directive.ts
+++ assignments/directives-start/src/app/better-highlight/better-hightlight.directive.ts
@@ -1,30 +1,36 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHightlight]'
 })
 export class BetterHightlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
-  @HostBinding('style.color') color: string = 'black';
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+  @Input() defaultTextColor: string = 'black';
+  @Input() highlightTextColor: string = 'white';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  @HostBinding('style.color') color: string = 'this.defaultTextColor';
   
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
   
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+    this.color = this.defaultTextColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
-    this.backgroundColor = 'blue';
-    this.color = 'white';
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.backgroundColor = this.highlightColor;
+    this.color = this.highlightTextColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
-    this.backgroundColor = 'transparent';
-    this.color = 'black';
+    this.backgroundColor = this.defaultColor;
+    this.color = this.defaultTextColor;
   }
 }
