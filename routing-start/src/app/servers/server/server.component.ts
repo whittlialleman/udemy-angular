040733c31db4ad6fc66319b8ba9df44382b728ff
whittlialleman <alle.alleman@gmail.com>
2023-07-11 10:35:59 -0600

    Finished video 142

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -24,7 +24,7 @@ export class ServerComponent implements OnInit {
   }
 
   onEdit() {
-    this.router.navigate(['edit'], { relativeTo: this.route});
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
 }
