49ad05b9d0866efd307b52a9d677dffb9ca1aeb7
whittlialleman <alle.alleman@gmail.com>
2023-07-11 11:30:13 -0600

    Finished video 150

--- routing-start/src/app/app-route.module.ts
+++ routing-start/src/app/app-route.module.ts
@@ -9,6 +9,7 @@ import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,7 +18,7 @@ const appRoutes: Routes = [
     ] },
     { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ] },
     { path: 'not-found', component: PageNotFoundComponent },
     { path: '**', redirectTo: '/not-found' } //wildcard route which catches all unknown pages and redirects to not found
