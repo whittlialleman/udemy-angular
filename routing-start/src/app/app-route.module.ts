8b63790a752448d396ea11dd9abc38194a12e4c0
whittlialleman <alle.alleman@gmail.com>
2023-07-11 11:55:42 -0600

    Finished video 153

--- routing-start/src/app/app-route.module.ts
+++ routing-start/src/app/app-route.module.ts
@@ -29,6 +29,7 @@ const appRoutes: Routes = [
   
 @NgModule({
     imports: [
+        // RouterModule.forRoot(appRoutes, {useHash: true})
         RouterModule.forRoot(appRoutes)
     ],
     exports: [RouterModule]
