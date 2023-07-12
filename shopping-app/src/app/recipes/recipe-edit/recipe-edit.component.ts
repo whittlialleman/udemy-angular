96e61290f69e1692362184ff05fb3b7a964c2098
whittlialleman <alle.alleman@gmail.com>
2023-07-12 17:18:33 -0600

    Finished video 232

--- shopping-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ shopping-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -29,6 +29,15 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
+
   get controls() { // a getter!
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
