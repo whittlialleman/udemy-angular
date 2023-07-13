ee414a57b265ae4ec00b885e349d56666ae76d87
whittlialleman <alle.alleman@gmail.com>
2023-07-13 10:07:39 -0600

    Finished video 259

--- http-01/src/app/app.component.ts
+++ http-01/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -16,9 +17,9 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
+    this.http.post<{name: string}>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
       console.log(responseData);
     });
   }
@@ -33,19 +34,20 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json')
+    this.http
+    .get<{[key: string]: Post }>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json')
     .pipe(map(responseData => {
-      const postsArray = [];
+      const postsArray: Post[] = [];
       for (const key in responseData) {
         if(responseData.hasOwnProperty(key)) {
-          postsArray.push({ ...responseData[key], id: key});
+          postsArray.push({ ...responseData[key], id: key });
         }
       }
       return postsArray;
     })
     )
     .subscribe(posts => {
-      console.log(posts);
+      console.log(posts[0]);
     });
   }
 }
