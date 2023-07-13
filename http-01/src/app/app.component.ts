119aae1e4ec4bf5ae57385fb0dc426c9215e4f6c
whittlialleman <alle.alleman@gmail.com>
2023-07-13 10:02:46 -0600

    Finished video 258

--- http-01/src/app/app.component.ts
+++ http-01/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -32,7 +33,18 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
+    this.http.get('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData => {
+      const postsArray = [];
+      for (const key in responseData) {
+        if(responseData.hasOwnProperty(key)) {
+          postsArray.push({ ...responseData[key], id: key});
+        }
+      }
+      return postsArray;
+    })
+    )
+    .subscribe(posts => {
       console.log(posts);
     });
   }
