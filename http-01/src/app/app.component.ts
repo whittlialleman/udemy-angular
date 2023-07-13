b0787027dcfde9a63e836013e4ed5beeeaaff8a1
whittlialleman <alle.alleman@gmail.com>
2023-07-13 10:23:37 -0600

    Finished video 262

--- http-01/src/app/app.component.ts
+++ http-01/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+
 import { Post } from './post.model';
+import { PostService } from './post.service';
 
 @Component({
   selector: 'app-root',
@@ -12,45 +14,23 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postService: PostService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postService.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post<{name: string}>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
-      console.log(responseData);
-    });
+    this.postService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.postService.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http
-    .get<{[key: string]: Post }>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json')
-    .pipe(map(responseData => {
-      const postsArray: Post[] = [];
-      for (const key in responseData) {
-        if(responseData.hasOwnProperty(key)) {
-          postsArray.push({ ...responseData[key], id: key });
-        }
-      }
-      return postsArray;
-    })
-    )
-    .subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
   }
 }
---
+++ http-01/src/app/post.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { map } from "rxjs/operators";
+
+@Injectable({providedIn: 'root'})
+export class PostService {
+
+    constructor(private http: HttpClient) {}
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = { title: title, content: content }
+        this.http
+            .post<{name: string}>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json', postData)
+            .subscribe(responseData => {
+                console.log(responseData);
+        });
+    }
+
+    fetchPosts() {
+        this.http
+        .get<{[key: string]: Post }>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json')
+        .pipe(map(responseData => {
+            const postsArray: Post[] = [];
+            for (const key in responseData) {
+                if(responseData.hasOwnProperty(key)) {
+                    postsArray.push({ ...responseData[key], id: key });
+                }
+            }
+            return postsArray;
+        })
+        )
+        .subscribe(posts => {
+        });
+    }
+}
