50d4c4c0b82ff754b98f9a53f7f7aad35b84fa94
whittlialleman <alle.alleman@gmail.com>
2023-07-13 10:55:47 -0600

    Finished video 270

--- http-01/src/app/post.service.ts
+++ http-01/src/app/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map, catchError } from "rxjs/operators";
@@ -22,9 +22,15 @@ export class PostService {
     }
 
     fetchPosts() {
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'pretty');
+        searchParams = searchParams.append('custom', 'key');
+
         return this.http
-        .get<{[key: string]: Post }>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json', {
-            headers: new HttpHeaders({ 'Custom-Header': 'Hello' })
+        .get<{[key: string]: Post }>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/posts.json', 
+        {
+            headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+            params: searchParams
         })
         .pipe(map(responseData => {
             const postsArray: Post[] = [];
