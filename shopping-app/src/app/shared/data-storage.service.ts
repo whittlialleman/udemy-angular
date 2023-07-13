376e1bff20c3d5fd62f7dc591134b39abdcd8f8e
whittlialleman <alle.alleman@gmail.com>
2023-07-13 14:44:51 -0600

    Finished video 283

--- shopping-app/src/app/shared/data-storage.service.ts
+++ shopping-app/src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({providedIn: 'root'}) //alternative to putting this in the providers array in app.module
 export class DataStorageService {
@@ -13,4 +14,9 @@ export class DataStorageService {
         });
     }
 
+    fetchRecipes() {
+        this.http.get<Recipe[]>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/recipes.json').subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
+        });
+    }
 }
