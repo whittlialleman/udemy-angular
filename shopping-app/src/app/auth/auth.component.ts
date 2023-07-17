207eeec22f6c9d6c261ed3e4074789353ff52249
whittlialleman <alle.alleman@gmail.com>
2023-07-17 14:20:48 -0600

    Finished video 301

--- shopping-app/src/app/auth/auth.component.ts
+++ shopping-app/src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -12,7 +13,7 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -37,7 +38,9 @@ export class AuthComponent {
     authObs.subscribe(resData => {
       console.log(resData);
       this.isLoading = false;
-    }, errorMessage => {
+      this.router.navigate(['/recipes']);
+    }, 
+    errorMessage => {
       console.log(errorMessage);
       this.error = errorMessage;
       this.isLoading = false;
