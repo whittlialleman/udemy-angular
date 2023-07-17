bc52d43ccf2c2875c0244f23febbfe0cb8220b34
whittlialleman <alle.alleman@gmail.com>
2023-07-17 14:31:48 -0600

    Finished video 302

--- shopping-app/src/app/shared/data-storage.service.ts
+++ shopping-app/src/app/shared/data-storage.service.ts
@@ -1,12 +1,13 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'}) //alternative to putting this in the providers array in app.module
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -16,16 +17,19 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
-        .get<Recipe[]>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/recipes.json')
-        .pipe(map(recipes => {
+        return this.authService.user.pipe(take(1), exhaustMap(user => {
+            return this.http
+            .get<Recipe[]>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/recipes.json', {
+                params: new HttpParams().set('auth', user.token)
+            });
+        }), 
+        map(recipes => {
             return recipes.map(recipe => {
                 return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
             });
         }), 
         tap(recipes => {
             this.recipeService.setRecipes(recipes);
-        })
-        )
+        }));
     }
 }
