c2e203c4da46921b0ccbb24ae063210151b1765a
whittlialleman <alle.alleman@gmail.com>
2023-07-17 13:14:45 -0600

    Finished video 297

--- shopping-app/src/app/auth/auth.servic.e.ts
+++ shopping-app/src/app/auth/auth.servic.e.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 interface AuthResponseData {
     idToken: string;
@@ -20,6 +22,17 @@ export class AuthService {
             email: email,
             password: password,
             returnSecureToken: true
-        });
+        })
+        .pipe(catchError(errorRes => {
+            let errorMessage = 'An unkown error occurred';
+            if (!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage);
+            }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'An account already exists with this email address';
+          }
+          return throwError(errorMessage);
+        }));
     }
 }
