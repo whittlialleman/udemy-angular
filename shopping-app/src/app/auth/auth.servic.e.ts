6eedcf6d0c1b7b0ba54f767fde073e500223319c
whittlialleman <alle.alleman@gmail.com>
2023-07-17 12:38:52 -0600

    Finished video 294

---
+++ shopping-app/src/app/auth/auth.servic.e.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    constructor(private http: HttpClient) {
+
+    }
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBzRRT8jh_EY8jXu2vu3YLyMy5mSa3ep8Y', {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
+    }
+}
