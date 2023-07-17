7b903e1741151a170fa73d047c2d5ab5306ea292
whittlialleman <alle.alleman@gmail.com>
2023-07-17 16:32:29 -0600

    Finished video 314

--- shopping-app/src/app/auth/auth.component.ts
+++ shopping-app/src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { AlertComponent } from '../shared/alert/alert.component';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -43,6 +44,7 @@ export class AuthComponent {
     errorMessage => {
       console.log(errorMessage);
       this.error = errorMessage;
+      this.showErrorAlert(errorMessage);
       this.isLoading = false;
     }
   );
@@ -52,5 +54,10 @@ export class AuthComponent {
 
   onHandleError() {
     this.error = null;
+  }
+
+  private showErrorAlert(message: string) {
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    
   }
 }
