96ec2d86be4052dbe939fb748d005c88e130acd1
whittlialleman <alle.alleman@gmail.com>
2023-07-17 14:43:11 -0600

    Finished video 303

--- shopping-app/src/app/shared/data-storage.service.ts
+++ shopping-app/src/app/shared/data-storage.service.ts
@@ -11,25 +11,25 @@ export class DataStorageService {
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
-        this.http.put('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response => {
+        this.http
+        .put('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/recipes.json', recipes)
+        .subscribe(response => {
             console.log(response);
         });
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(take(1), exhaustMap(user => {
             return this.http
-            .get<Recipe[]>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/recipes.json', {
-                params: new HttpParams().set('auth', user.token)
-            });
-        }), 
-        map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
-        }), 
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-        }));
+            .get<Recipe[]>('https://udemy-angular-f2c29-default-rtdb.firebaseio.com/recipes.json')
+            .pipe(
+                map(recipes => {
+                    return recipes.map(recipe => {
+                        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                    });
+                }), 
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+        );
     }
 }
