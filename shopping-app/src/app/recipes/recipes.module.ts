65889f68a75735a1e0c20e97ffe77d1949990f20
whittlialleman <alle.alleman@gmail.com>
2023-07-18 08:26:59 -0600

    Finished video 326

--- shopping-app/src/app/recipes/recipes.module.ts
+++ shopping-app/src/app/recipes/recipes.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 
 @NgModule({
     declarations: [
@@ -26,6 +28,11 @@ import { ReactiveFormsModule } from "@angular/forms";
     RecipeStartComponent,
     RecipeEditComponent,
     ],
-    imports: [RouterModule, CommonModule, ReactiveFormsModule]
+    imports: [
+    RouterModule, 
+    CommonModule, 
+    ReactiveFormsModule,
+    RecipesRoutingModule
+    ]
 })
 export class RecipesModule {}
