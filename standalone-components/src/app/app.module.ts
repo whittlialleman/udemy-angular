8bb9ea8938533bf08e13870ca09ba5da9f1720fb
whittlialleman <alle.alleman@gmail.com>
2023-07-18 11:28:48 -0600

    Finished video 350

--- standalone-components/src/app/app.module.ts
+++ standalone-components/src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { DetailsComponent } from './welcome/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent],
-  imports: [BrowserModule, DetailsComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- standalone-components/src/app/welcome/welcome.component.ts
+++ standalone-components/src/app/welcome/welcome.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
-//import { DetailsComponent } from './details/details.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
-  //imports: [DetailsComponent],
+  standalone: true,
+  imports: [DetailsComponent],
   selector: 'app-welcome',
   templateUrl: './welcome.component.html'
 })
