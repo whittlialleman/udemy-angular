c0704b90ada5bfd9d43b1151209d808be6fead04
whittlialleman <alle.alleman@gmail.com>
2023-07-19 08:20:37 -0600

    Finished video 361

--- signals/src/app/signals/signals.component.ts
+++ signals/src/app/signals/signals.component.ts
@@ -8,16 +8,17 @@ import { Component, signal } from '@angular/core';
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  actions = signal<string[]>([]);
   counter = signal(0);
 
   increment() {
-    this.counter.update((oldCounter) => oldCounter + 1);
-    this.actions.push('INCREMENT');
+    //this.counter.update((oldCounter) => oldCounter + 1); //different way of doing the same as below
+    this.counter.set(this.counter() + 1);
+    this.actions.mutate((oldActions) => oldActions.push('INCREMENT'))
   }
 
   decrement() {
     this.counter.update((oldCounter) => oldCounter - 1);
-    this.actions.push('DECREMENT');
+    this.actions.update((oldActions) => [...oldActions, 'DECREMENT']);
   }
 }
