dbb462057461b3d4c9a7a1a3b6dff3582c60c3a3
whittlialleman <alle.alleman@gmail.com>
2023-07-19 10:18:44 -0600

    Finished video 448

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -20,29 +20,49 @@ import { Component } from '@angular/core';
 
     trigger('wildState', [
       state('normal', style({
-        'background-color': 'red',
-        'transform': 'translateX(0) scale(1)'
+        backgroundColor: 'red',
+        transform : 'translateX(0) scale(1)'
       })),
       state('highlighted', style({
-        'background-color': 'blue',
-        'transform': 'translateX(100px) scale(1)'
+        backgroundColor: 'blue',
+        transform : 'translateX(100px) scale(1)'
       })),
       state('shrunken', style({
-        'background-color': 'green',
-        'transform': 'translateX(0) scale(0.5)'
+        backgroundColor: 'green',
+        transform : 'translateX(0) scale(0.5)'
       })),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
       transition('shrunken <=> *', [
         style({
-          'background-color': 'orange'
+          backgroundColor: 'orange'
         }),
         animate(1000, style({
           borderRadius: '50px'
         })),
         animate(500)
       ])
-    ])
+    ]),
+
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
   ]
 })
 export class AppComponent {
@@ -62,4 +82,8 @@ export class AppComponent {
     onAdd(item) {
       this.list.push(item);
     }
+
+    onDelete(item) {
+      this.list.splice(this.list.indexOf(item), 1);
+   }
 }
