012e440ccbe9c6b42ae359c5cf1c3a27446df371
whittlialleman <alle.alleman@gmail.com>
2023-07-19 08:07:24 -0600

    Copied over starter files for section 25 and finished video  358

---
+++ signals/src/app/signals/signals.component.ts
@@ -0,0 +1,23 @@
+import { NgFor } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: './signals.component.html',
+  standalone: true,
+  imports: [NgFor],
+})
+export class SignalsComponent {
+  actions: string[] = [];
+  counter = signal(0);
+
+  increment() {
+    this.counter++;
+    this.actions.push('INCREMENT');
+  }
+
+  decrement() {
+    this.counter--;
+    this.actions.push('DECREMENT');
+  }
+}
