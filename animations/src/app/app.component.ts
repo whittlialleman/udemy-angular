05ff1da12401741837d0ff32fc2b12858bc120d0
whittlialleman <alle.alleman@gmail.com>
2023-07-19 09:31:47 -0600

    Finished video 443

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        'transform': 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
